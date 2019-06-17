0e8113ce33b069878ac27a25c593b089c827a99a
muxanz <muxanzz@gmail.com>
2019-06-17 18:57:22 -0500

    0-bubble_sort.c file is added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Function that sorts an array of integers
+ *
+ * @array: The array to be sort
+ * @size: Number of elements in @array
+ */
+
+void bubble_sort(int *array, unsigned int size)
+{
+	unsigned int idx, stop = 0, c = 0, nord = 0;
+	int aux = 0;
+
+	stop = (size - 1) * (size - 1);
+	for (idx = 0; idx < (size - 1); idx++)
+	{
+		c++;
+		if (array[idx] > array[idx + 1])
+		{
+			aux = array[idx + 1];
+			array[idx + 1] = array[idx];
+			array[idx] = aux;
+			nord++;
+			print_array(array, size);
+		}
+
+		if (idx == size - 2 && nord > 0)
+		{
+			idx = -1;
+			nord = 0;
+		}
+
+		if (c == stop)
+			break;
+	}
+}
