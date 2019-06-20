b6f1437de96a0d849fdf7c7185ef71e85659759c
fesus rocuts <fesusrocuts@gmail.com>
2019-06-19 23:29:52 -0500

    update insert sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,35 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list used insert sort
+ * @list: Double pointer with nodes
+ * Return: void
+ */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *n_a, *n_b;
 
+	if (list == NULL || *list == NULL)
+		return;
+
+	n_a = *list;
+	while (n_a->next != NULL)
+	{
+		n_b = n_a->next;
+		while (n_a->prev != NULL && n_a->prev->n > n_a->n)
+		{
+			n_a->prev->next = n_a->next;
+			if (n_a->next != NULL)
+				n_a->next->prev = n_a->prev;
+			n_a->next = n_a->prev;
+			n_a->prev = n_a->next->prev;
+			n_a->next->prev = n_a;
+			if (n_a->prev == NULL)
+				*list = n_a;
+			else
+				n_a->prev->next = n_a;
+			print_list(*list);
+		}
+		n_a = n_b;
+	}
 }
